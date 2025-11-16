a8469c18664957697832118ae15a8efc590a012f
LinCie <aldiofernanda@protonmail.com>
2025-11-17 00:16:47 +0700

    feat(home): add partner logos section to homepage
    Add new PartnerLogo component and display partner logos in a dedicated section above the latest items. Includes logo assets for Haas, Hardinge, and Mazak.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
 import lang from "@/lang/id/home/lang";
 
 import { Button } from "@/components/ui/button";
-import { getItems } from "@/modules/items/items.service";
 import { ItemGrid } from "@/components/common/items/item-grid";
+import { PartnerLogo } from "@/components/home/partners-logo";
+import { getItems } from "@/modules/items/items.service";
 
 export default async function Home() {
   const items = await getItems({ limit: 4 });
 
   return (
-    <div className="mx-auto max-w-7xl px-1 py-3 md:px-6 md:py-10">
-      <h1 className="mb-4 text-center text-2xl font-extrabold uppercase md:mb-8 md:text-4xl">
-        {lang.titleLatestItems}
-      </h1>
-      <ItemGrid items={items.data} />
-      <Button
-        className="mx-auto mt-4 block md:mt-6"
-        size="lg"
-        variant="outline-primary"
-      >
-        Lihat Semua
-      </Button>
-    </div>
+    <>
+      <div className="bg-primary text-primary-foreground w-full">
+        <PartnerLogo />
+      </div>
+      <div className="mx-auto max-w-7xl px-1 py-3 md:px-6 md:py-10">
+        <h2 className="mb-4 text-center text-2xl font-extrabold uppercase md:mb-8 md:text-4xl">
+          {lang.titleLatestItems}
+        </h2>
+        <ItemGrid items={items.data} />
+        <Button
+          className="mx-auto mt-4 block md:mt-6"
+          size="lg"
+          variant="outline-primary"
+        >
+          Lihat Semua
+        </Button>
+      </div>
+    </>
   );
 }
---
+++ src/components/home/partners-logo.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+import mazak from "@/assets/logos/mazak.svg";
+import haas from "@/assets/logos/haas.png";
+import hardinge from "@/assets/logos/hardinge.png";
+
+function PartnerLogo() {
+  return (
+    <div className="mx-auto flex max-w-7xl items-center justify-around px-6 py-4 md:px-10 md:py-8">
+      <Image className="h-10 w-fit" src={mazak} alt="Yamazaki Mazak" />
+      <Image className="h-10 w-fit" src={haas} alt="Haas Automation" />
+      <Image className="h-10 w-fit" src={hardinge} alt="Hardinge" />
+    </div>
+  );
+}
+
+export { PartnerLogo };
