e3b12637d64746d33ce69de59285416d1456e2b1
LinCie <aldiofernanda@protonmail.com>
2025-11-16 21:15:48 +0700

    fix(env.config): correct enum value from 'itemion' to 'production'

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,7 +4,7 @@ const envSchema = z.object({
   ERP_URL: z.url(),
   ERP_SPACE: z.string(),
   NODE_ENV: z
-    .enum(["development", "itemion", "staging", "test"])
+    .enum(["development", "production", "staging", "test"])
     .default("development"),
 });
 
