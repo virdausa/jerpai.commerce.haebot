601083636fa65d955d6c04e30759667ba294016a
LinCie <aldiofernanda@protonmail.com>
2025-11-17 14:57:00 +0700

    style(home): adjust partner logos height for mobile responsiveness

--- src/components/home/partners-logo.tsx
+++ src/components/home/partners-logo.tsx
@@ -7,9 +7,9 @@ import hardinge from "@/assets/logos/hardinge.png";
 function PartnerLogo() {
   return (
     <div className="mx-auto flex max-w-7xl items-center justify-around px-6 py-4 md:px-10 md:py-8">
-      <Image className="h-10 w-fit" src={mazak} alt="Yamazaki Mazak" />
-      <Image className="h-10 w-fit" src={haas} alt="Haas Automation" />
-      <Image className="h-10 w-fit" src={hardinge} alt="Hardinge" />
+      <Image className="h-5 w-fit md:h-10" src={mazak} alt="Yamazaki Mazak" />
+      <Image className="h-5 w-fit md:h-10" src={haas} alt="Haas Automation" />
+      <Image className="h-5 w-fit md:h-10" src={hardinge} alt="Hardinge" />
     </div>
   );
 }
