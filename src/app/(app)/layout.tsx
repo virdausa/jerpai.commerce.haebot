2107fabf8871dc3bc8dfb570bdebed41854a5a35
LinCie <aldiofernanda@protonmail.com>
2025-11-22 22:02:58 +0700

    fix: fix `Missing Suspense boundary with useSearchParams` error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,12 @@
+import { connection } from "next/server";
 import { Breadcrumbs } from "@/features/navigation/components/breadcrumbs";
 
-export default function AppLayout({
+export default async function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  await connection();
   return (
     <div className="container mx-auto max-w-7xl px-1 py-3 md:px-6 md:py-10">
       <Breadcrumbs />
